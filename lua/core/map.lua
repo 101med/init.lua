86d0f825236bb6b3a1390d9a7ffd3c1408cf87d2
101med <med@void.localdomain>
2023-07-08 14:39:58 +0100

    Initial commit

---
+++ lua/core/map.lua
@@ -0,0 +1,31 @@
+vim.g.mapleader = " "
+
+local opts = { silent = true }
+
+vim.keymap.set("n", "<leader><leader>", ":Ex<CR>", opts)
+
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>", opts)
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>", opts)
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>", opts)
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>", opts)
+
+vim.keymap.set("n", "<leader>b", ":bn<CR>")
+vim.keymap.set("n", "<leader>B", ":bN<CR>")
+vim.keymap.set("n", "<leader>d", ":bd!<CR>")
+
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", opts)
+
+vim.keymap.set("n", "<leader>so", function()
+    vim.cmd("so")
+end)
---
+++ lua/core/set.lua
@@ -0,0 +1,34 @@
+local options = {
+    backup = false,
+    colorcolumn = "80",
+    cursorline = true,
+    expandtab = true,
+    hlsearch = false,
+    ignorecase = true,
+    incsearch = true,
+    mouse = "",
+    nu = true,
+    numberwidth = 4,
+    rnu = true,
+    scrolloff = 8,
+    shiftwidth = 4,
+    sidescrolloff = 16,
+    signcolumn = "yes",
+    --smartindent = true,
+    softtabstop = 4,
+    swapfile = false,
+    tabstop = 4,
+    --termguicolors = true,
+    undodir = os.getenv("XDG_DATA_HOME") .. "/nvim/undodir",
+    undofile = true,
+    updatetime = 60,
+    wrap = false,
+    writebackup = false,
+    laststatus = 3,
+    listchars = "eol:~,space:.",
+    list = true
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
